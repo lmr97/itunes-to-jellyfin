561e26d3dd9da265bc6edc822ad63560a0c229b5
lmr97 <lmreid1997@gmail.com>
2025-04-07 20:58:51 -0600

    warn output changes

--- xml-to-m3u.py
+++ xml-to-m3u.py
@@ -318,7 +318,8 @@ def parse_xml(cli_opts: dict):
                     if not os.path.exists(path):
 
                         print("\n\033[0;33mWarning\033[0m: unable to locate file:")
-                        print(f"\t'{get_str_attr(tr,"Name")}' by {get_str_attr(tr, "Artist")}")
+                        print(f"\t'{title}' by {artist}")
+                        print(f"Expected it at: {path}")
                         print("\033[0;33mWarning\033[0m: song not added to playlist")
 
                 elif cli_opts['check_exists'] == "error":
